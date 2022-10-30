84a304508a8f3417a24c9b7b314448050e09e711
Charles Cabergs <me@cacharle.xyz>
2022-10-30 14:17:21 +0100

    It works!

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #define _POSIX_C_SOURCE 200112L
+#include <stdarg.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/ip.h>
@@ -12,6 +14,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define DEFAULT_TTL 64
 
@@ -37,6 +40,16 @@
 **  1110001
 */
 
+void
+die(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
+}
+
 uint16_t
 checksum(uint16_t *data, size_t size)
 {
@@ -47,31 +60,52 @@ checksum(uint16_t *data, size_t size)
     return ~((sum << 16 >> 16) + (sum >> 16));
 }
 
+struct in_addr
+addrinfo_to_ip(const struct addrinfo *addrinfo)
+{
+    // from: https://stackoverflow.com/questions/20115295
+    struct sockaddr_in *addr = (struct sockaddr_in *)addrinfo->ai_addr;
+    return (struct in_addr)addr->sin_addr;
+}
+
+char *address = "google.com";
+
 int
 main()
 {
+    // gethostname();
     int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sock == -1)
-    {
-        perror("socket creation");
-        return 1;
-    }
+        die("ping: socket creation failed: %s\n", strerror(errno));
+    int on = 1;
+    if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) == -1)
+        die("ping: setsockopt failed: %s\n", strerror(errno));
 
-    int a[1] = {1};
-    if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, a, sizeof(int)) == -1)
+
+    struct addrinfo hints = {0};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = 0;
+    hints.ai_protocol = 0;
+    struct addrinfo *result;
+    if (getaddrinfo(address, NULL, &hints, &result) != 0)
+        die("ping: getaddrinfo failed: %s\n", strerror(errno));
+    for (struct addrinfo *r = result; r != NULL; r = r->ai_next)
     {
-    	perror("setsockopt");
-    	return 1;
+        char hostname[1084] = "";
+        getnameinfo(r->ai_addr, r->ai_addrlen, hostname, 1084, NULL, 0, 0);
+        struct in_addr addr = addrinfo_to_ip(r);
+        printf("hostname: %s (%s)\n", hostname, inet_ntoa(addr));
     }
+    struct in_addr dst_addr = addrinfo_to_ip(result);
 
     struct in_addr src_addr;
-    struct in_addr dst_addr;
-    inet_pton(AF_INET, "127.0.0.1", &src_addr);
-    inet_pton(AF_INET, "127.0.0.1", &dst_addr);
+    // struct in_addr dst_addr;
+    inet_pton(AF_INET, "192.168.1.42", &src_addr);
+    // inet_pton(AF_INET, address, &dst_addr);
 
-    printf("%x\n", sizeof(struct iphdr) + sizeof(struct icmphdr));
+    // printf("%x\n", sizeof(struct iphdr) + sizeof(struct icmphdr));
     struct iphdr ip_header = {
-        .ihl = 5,  // 20 byte = 160 bit; 160 / 32 = 5
+        .ihl = 5,  // Internet Header Length: 20 byte = 160 bit; 160 / 32 = 5
         .version = 4,
         .tos = 0,
         .tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr)),
@@ -104,19 +138,6 @@ main()
         printf("%02x ", msg_buf[sizeof(ip_header) + i]);
     printf("\n");
 
-    struct addrinfo *result;
-    if (getaddrinfo("127.0.0.1", NULL, NULL, &result) != 0)
-    {
-        perror("getaddrinfo");
-        return 1;
-    }
-    for (struct addrinfo *r = result; r != NULL; r = r->ai_next)
-    {
-        char hostname[1084] = "";
-        getnameinfo(r->ai_addr, r->ai_addrlen, hostname, 1084, NULL, 0, 0);
-        printf("hostname: %s\n", hostname);
-    }
-
     printf("sending\n");
     if (sendto(sock, msg_buf, sizeof(msg_buf), 0, result->ai_addr, result->ai_addrlen) == -1)
     {
@@ -146,6 +167,7 @@ main()
     // ip_header.tot_len = ntohs(ip_header.tot_len);
     // printf("ip_header.tot_len=%d\n", ip_header.tot_len);
 
+    freeaddrinfo(result);
     close(sock);
     return 0;
 }
