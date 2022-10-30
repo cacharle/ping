80a47aa005851e4f73d9df7686fcbacee56bba36
Charles Cabergs <me@cacharle.xyz>
2022-10-30 17:24:18 +0100

    Added alarm setup

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #define _POSIX_C_SOURCE 200112L
-#include <stdarg.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,7 +16,6 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <errno.h>
 
 #define DEFAULT_TTL 64
 
@@ -54,7 +55,7 @@ uint16_t
 checksum(uint16_t *data, size_t size)
 {
     uint32_t sum = 0;
-    size_t data_len = size / 2;
+    size_t   data_len = size / 2;
     for (size_t i = 0; i < data_len; i++)
         sum += data[i];
     return ~((sum << 16 >> 16) + (sum >> 16));
@@ -68,98 +69,141 @@ addrinfo_to_ip(const struct addrinfo *addrinfo)
     return (struct in_addr)addr->sin_addr;
 }
 
+struct ping_packet
+{
+    struct iphdr   ip;
+    struct icmphdr icmp;
+};
+
+static int                sock = -1;
+static struct ping_packet send_packet;
+static struct ping_packet recv_packet;
+static struct addrinfo   *destination_addrinfo;
+struct in_addr            dst_addr;
+struct in_addr            src_addr;
+
+void
+ping(int signalnum)
+{
+    (void)signalnum;
+
+    // printf("%x\n", sizeof(struct iphdr) + sizeof(struct icmphdr));
+    send_packet.ip.ihl =
+        5,  // Internet Header Length: 20 byte = 160 bit; 160 / 32 = 5
+        send_packet.ip.version = 4, send_packet.ip.tos = 0,
+    send_packet.ip.tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr)),
+    send_packet.ip.id = 0, send_packet.ip.frag_off = 0,
+    send_packet.ip.ttl = DEFAULT_TTL, send_packet.ip.protocol = IPPROTO_ICMP,
+    send_packet.ip.check = 0, send_packet.ip.saddr = src_addr.s_addr,
+    send_packet.ip.daddr = dst_addr.s_addr,
+    send_packet.ip.check =
+        checksum((uint16_t *)&send_packet.ip, sizeof(send_packet.ip));
+
+    send_packet.icmp.type = ICMP_ECHO, send_packet.icmp.code = 0,
+    send_packet.icmp.checksum = 0, send_packet.icmp.un.echo.id = 0,
+    send_packet.icmp.un.echo.sequence = 1,
+    send_packet.icmp.checksum =
+        checksum((uint16_t *)&send_packet.icmp, sizeof(send_packet.icmp));
+
+    // uint8_t msg_buf[sizeof(struct iphdr) + sizeof(struct icmphdr)];
+    // memcpy(msg_buf, &ip_header, sizeof(struct iphdr));
+    // memcpy(msg_buf + sizeof(struct iphdr), &icmp_header, sizeof(struct icmphdr));
+    // for (int i = 0; i < sizeof(ip_header); i++)
+    //     printf("%02x ", msg_buf[i]);
+    // printf(" | ");
+    // for (int i = 0; i < sizeof(icmp_header); i++)
+    //     printf("%02x ", msg_buf[sizeof(ip_header) + i]);
+    // printf("\n");
+
+    if (sendto(sock,
+               &send_packet,
+               sizeof(send_packet),
+               0,
+               destination_addrinfo->ai_addr,
+               destination_addrinfo->ai_addrlen) == -1)
+        die("");
+    signal(SIGALRM, ping);
+    alarm(1);
+}
+
 char *address = "google.com";
 
 int
 main()
 {
     // gethostname();
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sock == -1)
         die("ping: socket creation failed: %s\n", strerror(errno));
     int on = 1;
     if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) == -1)
         die("ping: setsockopt failed: %s\n", strerror(errno));
 
-
     struct addrinfo hints = {0};
     hints.ai_family = AF_INET;
     hints.ai_socktype = 0;
     hints.ai_protocol = 0;
-    struct addrinfo *result;
-    if (getaddrinfo(address, NULL, &hints, &result) != 0)
+    if (getaddrinfo(address, NULL, &hints, &destination_addrinfo) != 0)
         die("ping: getaddrinfo failed: %s\n", strerror(errno));
-    for (struct addrinfo *r = result; r != NULL; r = r->ai_next)
-    {
-        char hostname[1084] = "";
-        getnameinfo(r->ai_addr, r->ai_addrlen, hostname, 1084, NULL, 0, 0);
-        struct in_addr addr = addrinfo_to_ip(r);
-        printf("hostname: %s (%s)\n", hostname, inet_ntoa(addr));
-    }
-    struct in_addr dst_addr = addrinfo_to_ip(result);
+    char destination_hostname[1048] = "";
+    getnameinfo(destination_addrinfo->ai_addr,
+                destination_addrinfo->ai_addrlen,
+                destination_hostname,
+                1048,
+                NULL,
+                0,
+                0);
+    // for (struct addrinfo *r = destination_addrinfo; r != NULL; r = r->ai_next)
+    // {
+    //     char hostname[1084] = "";
+    //     getnameinfo(r->ai_addr, r->ai_addrlen, hostname, 1084, NULL, 0, 0);
+    //     struct in_addr addr = addrinfo_to_ip(r);
+    //     printf("hostname: %s (%s)\n", hostname, inet_ntoa(addr));
+    // }
 
-    struct in_addr src_addr;
-    // struct in_addr dst_addr;
+    dst_addr = addrinfo_to_ip(destination_addrinfo);
     inet_pton(AF_INET, "192.168.1.42", &src_addr);
     // inet_pton(AF_INET, address, &dst_addr);
 
-    // printf("%x\n", sizeof(struct iphdr) + sizeof(struct icmphdr));
-    struct iphdr ip_header = {
-        .ihl = 5,  // Internet Header Length: 20 byte = 160 bit; 160 / 32 = 5
-        .version = 4,
-        .tos = 0,
-        .tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr)),
-        .id = 0,
-        .frag_off = 0,
-        .ttl = DEFAULT_TTL,
-        .protocol = IPPROTO_ICMP,
-        .check = 0,
-        .saddr = src_addr.s_addr,
-        .daddr = dst_addr.s_addr,
-    };
-    ip_header.check = checksum((uint16_t *)&ip_header, sizeof(ip_header));
+    signal(SIGALRM, ping);
+    ping(0);
 
-    struct icmphdr icmp_header = {
-        .type = ICMP_ECHO,
-        .code = 0,
-        .checksum = 0,
-        .un.echo.id = 0,
-        .un.echo.sequence = 1,
-    };
-    icmp_header.checksum = checksum((uint16_t *)&icmp_header, sizeof(icmp_header));
-
-    uint8_t msg_buf[sizeof(struct iphdr) + sizeof(struct icmphdr)];
-    memcpy(msg_buf, &ip_header, sizeof(struct iphdr));
-    memcpy(msg_buf + sizeof(struct iphdr), &icmp_header, sizeof(struct icmphdr));
-    for (int i = 0; i < sizeof(ip_header); i++)
-        printf("%02x ", msg_buf[i]);
-    printf(" | ");
-    for (int i = 0; i < sizeof(icmp_header); i++)
-        printf("%02x ", msg_buf[sizeof(ip_header) + i]);
-    printf("\n");
-
-    printf("sending\n");
-    if (sendto(sock, msg_buf, sizeof(msg_buf), 0, result->ai_addr, result->ai_addrlen) == -1)
+    while (true)
     {
-        perror("send");
-        close(sock);
-        return 1;
+        if (recvfrom(sock,
+                     &recv_packet,
+                     sizeof(recv_packet),
+                     0,
+                     destination_addrinfo->ai_addr,
+                     &destination_addrinfo->ai_addrlen) == -1)
+            continue;
+        printf("%zu bytes from %s (%s): icmp_seq=%d ttl=%d time=%.1f ms\n",
+               sizeof(recv_packet),
+               destination_hostname,
+               inet_ntoa(dst_addr),
+               1,
+               1,
+               1.1);
+        // die("recv: %s", strerror(errno));
+        // for (int i = 0; i < sizeof(recv_packet.ip); i++)
+        //     printf("%02x ", ((uint8_t*)&recv_packet.ip)[i]);
+        // printf(" | ");
+        // for (int i = 0; i < sizeof(recv_packet.icmp); i++)
+        //     printf("%02x ", ((uint8_t*)&recv_packet.ip)[sizeof(recv_packet.ip) +
+        //     i]);
+        // printf("\n");
     }
 
-    memset(msg_buf, 0, sizeof(msg_buf));
-    printf("receiving\n");
-    if (recvfrom(sock, &msg_buf, sizeof(msg_buf), 0, result->ai_addr, &result->ai_addrlen) == -1)
-    {
-        perror("recv");
-        close(sock);
-        return 1;
-    }
-    for (int i = 0; i < sizeof(ip_header); i++)
-        printf("%02x ", msg_buf[i]);
-    printf(" | ");
-    for (int i = 0; i < sizeof(icmp_header); i++)
-        printf("%02x ", msg_buf[sizeof(ip_header) + i]);
-    printf("\n");
+    // f {
+    //     send();
+    //     signal(sigalrm, f);
+    //     alarm(interval_time);
+    // }
+    //
+    // f()
+    // while (1) {
+    //     recv
+    // }
 
     // memcpy(&ip_header, msg_buf, sizeof(ip_header));
     // memcpy(&icmp_header, msg_buf + sizeof(ip_header), sizeof(icmp_header));
@@ -167,7 +211,7 @@ main()
     // ip_header.tot_len = ntohs(ip_header.tot_len);
     // printf("ip_header.tot_len=%d\n", ip_header.tot_len);
 
-    freeaddrinfo(result);
+    freeaddrinfo(destination_addrinfo);
     close(sock);
     return 0;
 }
